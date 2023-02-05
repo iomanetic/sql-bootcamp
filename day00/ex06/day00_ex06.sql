556a4e7eaa13a66c89b7a204fa7c486b5f7f4945
Karim Valiev <karimvaliev78@yandex.ru>
2023-02-05 23:50:53 +0300

    ex succes

--- day00/ex06/day00_ex06.sql
+++ day00/ex06/day00_ex06.sql
@@ -6,4 +6,4 @@ SELECT
      FROM person
      WHERE person.id = person_order.person_id) = 'Denis' AS condition
 FROM person_order
-WHERE (menu_id = 13 OR menu_id = 14 OR menu_id = 18) AND order_date = '2022-01-07'
+WHERE (menu_id = 13 OR menu_id = 14 OR menu_id = 18) AND order_date = '2022-01-07';
