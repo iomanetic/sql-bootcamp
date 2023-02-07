55a7f67fa5a1d3ecf83cd64c206a4a0764979c87
Karim Valiev <karimvaliev.qq@gmail.com>
2023-02-07 20:59:57 +0300

    ex08 succes

---
+++ day02/ex08/day02_ex08.sql
@@ -0,0 +1,7 @@
+SELECT p.name AS name
+FROM person p
+    JOIN person_order po on p.id = po.person_id
+    JOIN menu m on po.menu_id = m.id
+WHERE m.pizza_name IN ('pepperoni pizza', 'mushroom pizza')
+     AND p.address IN ('Moscow', 'Samara') AND p.gender = 'male'
+ORDER BY name DESC;
