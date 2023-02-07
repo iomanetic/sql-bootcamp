3093c331095e0b524d2f9899ae87da34064d1b8a
Karim Valiev <karimvaliev.qq@gmail.com>
2023-02-07 19:23:15 +0300

    exes succes

--- day02/ex03/day02_ex03.sql
+++ day02/ex03/day02_ex03.sql
@@ -1,10 +1,16 @@
-WITH cte AS (
-    SELECT visit_date
-    FROM person_visits
-    WHERE person_id = 1 OR person_id = 2
-)
-SELECT missing_date::date
-FROM generate_series('2022-01-01', '2022-01-10', interval '1 day') as missing_date
-LEFT JOIN cte ON cte.visit_date = missing_date::date
-WHERE cte.visit_date IS NULL
+WITH cte AS
+         (
+             SELECT missing_date::date
+             FROM generate_series('2022-01-01', '2022-01-10', interval '1 day') AS missing_date
+         )
+SELECT cte.missing_date::date
+FROM cte
+         LEFT JOIN
+     (
+         SELECT visit_date
+         FROM person_visits
+         WHERE person_id = 1 OR person_id = 2
+     ) AS pv
+     ON pv.visit_date = cte.missing_date::date
+WHERE pv.visit_date IS NULL
 ORDER BY missing_date;
