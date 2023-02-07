eb61d6a0b07f759300d2a0c6da932907d4eb9271
Karim Valiev <karimvaliev.qq@gmail.com>
2023-02-08 01:13:59 +0300

    ex02 succes

---
+++ day03/ex02/day03_ex02.sql
@@ -0,0 +1,17 @@
+SELECT m.pizza_name AS pizza_name,
+       m.price AS price,
+       piz.name AS pizzeria_name
+FROM person_order AS po
+    FULL JOIN menu m ON po.menu_id = m.id
+    JOIN pizzeria piz ON m.pizzeria_id = piz.id
+WHERE m.id IN
+    ((
+        SELECT id AS menu_id
+        FROM menu
+        EXCEPT
+        SELECT menu_id
+        FROM person_order
+        ORDER BY menu_id
+    ))
+ORDER BY pizza_name,
+         price;
