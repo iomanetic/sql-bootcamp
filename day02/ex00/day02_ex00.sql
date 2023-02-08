4e56e3ba294725fb2866c0139c04fcb543743d38
Karim Valiev <karimvaliev.qq@gmail.com>
2023-02-08 15:18:22 +0300

    ex03 succes

--- day02/ex00/day02_ex00.sql
+++ day02/ex00/day02_ex00.sql
@@ -1,4 +1,5 @@
-SELECT name AS pizzeria_name
+SELECT name AS pizzeria_name,
+       rating AS rating
 FROM pizzeria
     LEFT JOIN person_visits pv ON pizzeria.id = pv.pizzeria_id
 WHERE pv.pizzeria_id IS NULL
---
+++ day03/ex03/day03_ex03.sql
@@ -0,0 +1,29 @@
+WITH
+    female_pizzerias AS
+    (
+        SELECT piz.name AS pizzeria_name
+        FROM person p
+            JOIN person_visits pv on p.id = pv.person_id
+            JOIN pizzeria piz on pv.pizzeria_id = piz.id
+        WHERE p.gender = 'female'
+    ),
+    male_pizzerias AS
+    (
+        SELECT piz.name AS pizzeria_name
+        FROM person p
+            JOIN person_visits pv on p.id = pv.person_id
+            JOIN pizzeria piz on pv.pizzeria_id = piz.id
+        WHERE p.gender = 'male'
+    )
+(SELECT female_pizzerias.pizzeria_name
+FROM female_pizzerias
+EXCEPT ALL
+SELECT male_pizzerias.pizzeria_name
+FROM male_pizzerias)
+UNION ALL
+(SELECT male_pizzerias.pizzeria_name
+FROM male_pizzerias
+EXCEPT ALL
+SELECT female_pizzerias.pizzeria_name
+FROM female_pizzerias)
+ORDER BY pizzeria_name;
