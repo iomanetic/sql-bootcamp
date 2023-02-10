6ded83328d6ff2dc14035e9c532e05d3a1fd7707
Karim Valiev <karimvaliev.qq@gmail.com>
2023-02-10 23:36:26 +0300

    ex01 succes

---
+++ day06/ex01/day06_ex01.sql
@@ -0,0 +1,14 @@
+INSERT INTO person_discounts (id, person_id, pizzeria_id, discount)
+SELECT row_number() over () AS id,
+       person_id AS person_id,
+       m.pizzeria_id AS pizzeria_id,
+       CASE
+           WHEN (count(*) = 1)
+           THEN 10.5
+           WHEN (count(*) = 2)
+           THEN 22
+           ELSE 30
+       END AS discount
+FROM person_order
+    JOIN menu m on person_order.menu_id = m.id
+GROUP BY person_id, pizzeria_id;
