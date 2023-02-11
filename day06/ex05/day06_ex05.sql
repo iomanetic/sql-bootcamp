ebc751d6a2aebade4a293c5ce3082d2083b92763
Karim Valiev <karimvaliev.qq@gmail.com>
2023-02-11 20:48:58 +0300

    ex05 succes

---
+++ day06/ex05/day06_ex05.sql
@@ -0,0 +1,4 @@
+COMMENT ON COLUMN person_discounts.id IS 'unique column id';
+COMMENT ON COLUMN person_discounts.person_id IS 'id of the person who has the discount';
+COMMENT ON COLUMN person_discounts.pizzeria_id IS 'id of pizzeria when person has discount';
+COMMENT ON COLUMN person_discounts.discount IS 'the amount of a person''s discount as a percentage';
