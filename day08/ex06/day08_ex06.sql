04acca0a0638ba1293b390e49092327e8a1f179f
Karim Valiev <karimvaliev.qq@gmail.com>
2023-02-15 00:16:41 +0300

    ex06 succes

---
+++ day08/ex06/day08_ex06.sql
@@ -0,0 +1,23 @@
+-- Session 1
+begin transaction isolation level repeatable read;
+
+-- Session 2
+begin transaction isolation level repeatable read;
+
+-- Session 1
+select sum(rating) from pizzeria;
+
+-- Session 2
+update pizzeria set rating=5 where name='Pizza Hut';
+commit;
+
+-- Session 1
+select sum(rating) from pizzeria;
+commit;
+
+-- Session 1
+select sum(rating) from pizzeria;
+
+-- Session 2
+select sum(rating) from pizzeria;
+
