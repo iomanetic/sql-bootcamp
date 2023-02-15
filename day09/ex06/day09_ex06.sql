e16015b324cd5070cec8e0e3c5a74471d409011e
Karim Valiev <karimvaliev.qq@gmail.com>
2023-02-16 01:26:30 +0300

    ex succes

---
+++ day09/ex06/day09_ex06.sql
@@ -0,0 +1,28 @@
+CREATE OR REPLACE FUNCTION fnc_person_visits_and_eats_on_date(IN pperson VARCHAR DEFAULT 'Dmitriy',
+                                                              IN pprice NUMERIC DEFAULT 500,
+                                                              IN pdate DATE DEFAULT '2022-01-08')
+                                                RETURNS TABLE
+                                                (
+                                                    name VARCHAR
+                                                )
+LANGUAGE plpgsql
+AS
+$$
+    BEGIN
+        RETURN QUERY (SELECT piz.name AS pizzeria_name
+        FROM person AS p
+            JOIN person_visits pv on p.id = pv.person_id
+            JOIN person_order po on p.id = po.person_id AND po.order_date = pv.visit_date
+            JOIN menu m on po.menu_id = m.id
+            JOIN pizzeria piz on m.pizzeria_id = piz.id
+        WHERE p.name = pperson
+              AND m.price < pprice
+              AND po.order_date = pdate);
+    END;
+$$;
+
+select *
+from fnc_person_visits_and_eats_on_date2(pprice := 800);
+
+select *
+from fnc_person_visits_and_eats_on_date(pperson := 'Anna',pprice := 1300,pdate := '2022-01-01');
---
+++ day09/ex07/day09_ex07.sql
@@ -0,0 +1,9 @@
+CREATE FUNCTION func_minimum(VARIADIC arr numeric[]) RETURNS NUMERIC
+LANGUAGE sql
+AS
+$$
+    SELECT MIN(arr[i])
+    FROM generate_subscripts(arr, 1) g(i);
+$$;
+
+SELECT func_minimum(VARIADIC arr => ARRAY[10.0, -1.0, 5.0, 4.4]);
